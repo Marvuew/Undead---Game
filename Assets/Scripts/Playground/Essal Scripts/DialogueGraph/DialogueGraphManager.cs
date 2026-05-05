ec91db36e6cf6a7ae10edc266252ae6c80d6969d
Mathilde Lawaetz <mathilde.lawaetz@gmail.com>
2026-05-05 17:48:52 +0200

    made something

--- Assets/Scripts/Playground/Essal Scripts/DialogueGraph/DialogueGraphManager.cs
+++ Assets/Scripts/Playground/Essal Scripts/DialogueGraph/DialogueGraphManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
