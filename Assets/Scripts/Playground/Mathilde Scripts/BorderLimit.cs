4c0d104fbd23e05fa11d659d8b1d242ee700d0ad
Mathilde Lawaetz <mathilde.lawaetz@gmail.com>
2026-05-20 12:17:43 +0200

    house colliders + no one is home text

--- Assets/Scripts/Playground/Mathilde Scripts/BorderLimit.cs
+++ Assets/Scripts/Playground/Mathilde Scripts/BorderLimit.cs
@@ -7,10 +7,10 @@ public class BorderLimit : MonoBehaviour
 
 
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
 
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             DialogueGraphManager.instance.StartDialogue(nodeToStart);
         }
